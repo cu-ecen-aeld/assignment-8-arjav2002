ffc2dbfeafcd291cb9c186f8f145b62cc0fe9b48
Arjav Garg <arjavgarg@gmail.com>
2025-06-25 17:26:36 +0530

    works 6-2

--- runqemu.sh
+++ runqemu.sh
@@ -2,5 +2,7 @@
 # Script to start qemu
 
 source poky/oe-init-build-env
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
 export QB_SLIRP_OPT="-netdev user,id=net0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000"
 runqemu slirp nographic
